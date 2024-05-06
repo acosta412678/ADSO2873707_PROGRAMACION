2132a40d10bcde60050460e5e5af469f120de90d
acosta412678 <luismiguel412678@gmail.com>
2024-05-06 14:53:33 -0500

    Modificcación de FOR02, nuevos archivos FOR03 y FOR04 y eliminacion de eje03

--- Ejercicios Dart/Ciclos/EjemplosCiclos/ejemploFOR02.dart
+++ Ejercicios Dart/Ciclos/EjemplosCiclos/ejemploFOR02.dart
@@ -1,3 +1,6 @@
 void main(List<String> args) {
-  
+  //Imprimir los numeros del 5 al 14
+  for (int i = 5; i < 15; i++) {
+    print("i: $i");
+  }
 }
---
+++ Ejercicios Dart/Ciclos/EjemplosCiclos/ejemploFOR03.dart
@@ -0,0 +1,9 @@
+import 'dart:ffi';
+
+void main(List<String> args) {
+  //Imprimir los números del 1 al 25, de 2 en 2
+  print("Imprimir los números del 1 al 25, de 2 en 2");
+  for (int i = 1; i <= 25; i += 2) {
+    print("i: $i");
+  }
+}
