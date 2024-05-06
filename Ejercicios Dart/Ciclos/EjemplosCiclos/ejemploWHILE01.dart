4cabf6e500e7269a836ba58b3346d7ee59ddd0d0
acosta412678 <luismiguel412678@gmail.com>
2024-05-06 15:27:20 -0500

    Se agrega 1 ejemplo de WHILE

---
+++ Ejercicios Dart/Ciclos/EjemplosCiclos/ejemploWHILE01.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  // WHILE: Eejmplo donde se conoce la cantidad de interacciones
+  // Variable de tipo CONTADOR
+  int contador = 0;
+  while (contador <= 5) {
+    print("Valor: $contador");
+    contador++; // Se incrementa 1
+  }
+}
