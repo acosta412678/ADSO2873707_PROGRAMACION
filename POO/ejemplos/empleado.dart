7fdf1bdb5031796105da46fbe61dbef5d2236457
acosta412678 <160658635+acosta412678@users.noreply.github.com>
2024-08-06 21:06:06 -0500

    Add files via upload

--- POO/ejemplos/empleado.dart
+++ POO/ejemplos/empleado.dart
@@ -6,6 +6,23 @@ class Empleado {
   String tipoContrato;
 
   // Constructor
+<<<<<<< HEAD
+  Empleado(
+      this.nombre, this.edad, this.salario, this.puesto, this.tipoContrato);
+
+  // Método para aumentar el salario
+  void aumentarSalario(double porcentaje) {
+    this.salario += this.salario * (porcentaje / 100);
+  }
+
+  // Método para cumplir años
+  cumplirAnios() {
+    this.edad++;
+  }
+
+  // Método para cambiar el puesto
+  cambiarPuesto(String nuevoPuesto) {
+=======
   Empleado(this.nombre, this.edad, this.salario, this.puesto, this.tipoContrato);
 
   // Método para aumentar el salario
@@ -20,21 +37,37 @@ class Empleado {
 
   // Método para cambiar el puesto
   void cambiarPuesto(String nuevoPuesto) {
+>>>>>>> d929e2410b558637c76a039a8c716c02adfb8876
     puesto = nuevoPuesto;
   }
 
   // Método para mostrar la información
   void mostrarInformacion() {
+<<<<<<< HEAD
+    print("""
+      Nombre: $nombre.
+      Edad: $edad.
+      Salario: $salario.
+      Puesto: $puesto.
+      Tipo Contrato: $tipoContrato
+  """);
+=======
     print('Nombre: $nombre');
     print('Edad: $edad');
     print('Salario: \$${salario.toStringAsFixed(2)}');
     print('Puesto: $puesto');
+>>>>>>> d929e2410b558637c76a039a8c716c02adfb8876
   }
 
   // Método para calcular la bonificación
   double calcularBonificacion() {
+<<<<<<< HEAD
+    double bonificacion = 0;
+    switch (tipoContrato.toLowerCase()) {
+=======
     double bonificacion = 0.0;
     switch (tipoContrato) {
+>>>>>>> d929e2410b558637c76a039a8c716c02adfb8876
       case 'Contratista':
         bonificacion = salario * 0.10;
         break;
--- POO/ejemplos/main.dart
+++ POO/ejemplos/main.dart
@@ -1,4 +1,51 @@
 import 'dart:io';
+<<<<<<< HEAD
+import 'empleado.dart';
+
+void main(List<String> args) {
+  List<Empleado> empleados = [];
+
+  print('Ingrese el número de empleados a recolectar información:');
+  int numEmpleados = int.parse(stdin.readLineSync()!);
+
+  // Recolectar la información de los empleados
+  for (int i = 0; i < numEmpleados; i++) {
+    print('Ingrese los datos del empleado ${i + 1}:');
+
+    print('Nombre:');
+    String nombre = stdin.readLineSync()!;
+
+    print('Edad:');
+    int edad = int.parse(stdin.readLineSync()!);
+
+    print('Salario:');
+    double salario = double.parse(stdin.readLineSync()!);
+
+    print('Puesto:');
+    String puesto = stdin.readLineSync()!;
+
+    print('Tipo de contrato (Indefinido, Temporal, Contratista):');
+    String tipoContrato = stdin.readLineSync()!;
+
+    // Crear una instancia de Empleado y añadir a la lista
+    Empleado empleado = Empleado(nombre, edad, salario, puesto, tipoContrato);
+    empleados.add(empleado);
+  }
+
+  // Mostrar la información de cada empleado
+  print('Información de los empleados:');
+  for (int i = 0; i < empleados.length; i++) {
+    Empleado empleado = empleados[i];
+    print('Empleado ${i + 1}:');
+    empleado.mostrarInformacion();
+    double bonificacion = empleado.calcularBonificacion();
+    print('Bonificación: $bonificacion');
+    print('Ingrese el nuevo puesto:');
+    String nuevoPuesto = stdin.readLineSync()!;
+    empleado.cambiarPuesto(nuevoPuesto);
+    empleado.cumplirAnios();
+    empleado.mostrarInformacion();
+=======
 
 import 'empleado.dart';
 
@@ -53,5 +100,6 @@ void main() {
     print('Información actualizada del empleado:');
     empleado.mostrarInformacion();
     print('Bonificación actualizada: \$${empleado.calcularBonificacion().toStringAsFixed(2)}\n');
+>>>>>>> d929e2410b558637c76a039a8c716c02adfb8876
   }
 }
