b9ec94bf5cbf3bb100dd927adb5e990008e804f2
unknown <SENA@MANAPRCPIFSD001.FormacionSena.RED>
2024-10-22 17:40:41 -0500

    A

--- flutter/ejemplosflutter/flutter_app_05/lib/main.dart
+++ flutter/ejemplosflutter/flutter_app_05/lib/main.dart
@@ -8,27 +8,66 @@ class Main extends StatefulWidget {
   const Main({super.key});
 
   @override
-  State<Main> createState() => _MainState(); // SE SOBREESCRIBE EL METODO CREATESTATE DEL PADRE PARA CREAR EL ESTADO 
+  State<Main> createState() => _MainState();
 }
 
 class _MainState extends State<Main> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "hola, adios",
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Sitios turisticos"),
-          backgroundColor: const Color.fromARGB(255, 0, 171, 201),
-          foregroundColor: Colors.white,
+          title: const Text("Grillas"),
         ),
-      
-        body: ListView(
-          children: const [
 
-          ],
+        body: Container(
+          color: Colors.white,
+          child: GridView.count(
+            crossAxisSpacing: 9,
+            mainAxisSpacing: 9,
+            crossAxisCount: 4,
+            children:  const [
+              Card(
+                child: Column(
+                  children: [
+                    Text("targeta 1"),
+                    Icon(Icons.access_time_filled_rounded),
+                    Text("HELLO, MY NAME IS Andres Felipe Rivera Sanchez"),
+                  ],
+                ),
+              ),
+              Card(
+                child: Text("targeta 2"),
+              ),
+              Card(
+                child: Text("targeta 3"),
+              ),
+              Card(
+                child: Text("targeta 4"),
+              ),
+              Card(
+                child: Text("targeta 5"),
+              ),
+              Card(
+                child: Text("targeta 6"),
+              ),
+              Card(
+                child: Text("targeta 7"),
+              ),
+              Card(
+                child: Text("targeta 8"),
+              ),
+              Card(
+                child: Text("targeta 9"),
+              ),
+              Card(
+                child: Text("targeta 10"),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
